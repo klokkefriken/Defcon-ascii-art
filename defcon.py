3259b49a386915c19f3020244d3da02ac66e4726
klokkefriken <klokkefriken2@protonmail.com>
2020-09-21 01:15:19 +0200

    first commit

---
+++ defcon.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def printMsg():
+    str = """                                          
+    ,'   Y`.                                
+   /        \                                
+   \ ()  () /       IS DEFCON SOME SORT OF   
+    `. /\ ,'        CONFERENCE FOR RAPPERS?
+8====| "" |====8                             
+     `LLLU'                                  
+"""
+    print(str)
+printMsg()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
