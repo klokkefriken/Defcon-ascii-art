9a56b8de2e1841b58aa581d26ce5aed208e6f937
klokkefriken <klokkefriken2@protonmail.com>
2020-09-21 01:37:26 +0200

    fixed stuff

--- defcon.py
+++ defcon.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -15,40 +16,5 @@ def printMsg():
 """
     print(str)
 printMsg()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
